7d9b0e5a3369cd4dd25bd1a33b7e9d54ac7d5c84
danielboros78 <danielboros78@gmail.com>
2021-10-05 10:17:30 +0200

    Add project files.

---
+++ Elem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kémia
+{
+  class Elem
+  {
+    private string ev;
+
+    public string Ev
+    {
+      get { return ev; }
+    }
+
+    private string nev;
+
+    public string Nev
+    {
+      get { return nev; }
+    }
+
+    private string vegyjel;
+
+    public string Vegyjel
+    {
+      get { return vegyjel; }
+    }
+
+    private int rendszam;
+
+    public int Rendszam
+    {
+      get { return rendszam; }
+    }
+
+    private string tudos;
+
+    public string Tudos
+    {
+      get { return tudos; }
+    }
+
+    public Elem(string ev, string nev, string vegyjel, int rendszam, string tudos)
+    {
+      this.ev = ev;
+      this.nev = nev;
+      this.vegyjel = vegyjel;
+      this.rendszam = rendszam;
+      this.tudos = tudos;
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Kémia
+{
+  class Program
+  {
+    static List<Elem> elemek = new List<Elem>();
+    static void Main(string[] args)
+    {
+      MasodikFeladat();
+      HarmadikFeladat();
+      Console.ReadKey();
+    }
+
+    private static void HarmadikFeladat()
+    {
+      Console.WriteLine($"3. feladat: Elemek száma: {elemek.Count}");
+    }
+
+    private static void MasodikFeladat()
+    {
+      StreamReader be = new StreamReader("felfedezesek.csv");
+      be.ReadLine();
+      while (!be.EndOfStream)
+      {
+        string[] a = new string[5];
+        a = be.ReadLine().Split(';');
+        elemek.Add(new Elem(a[0], a[1], a[2], Convert.ToInt32(a[3]), a[4]));
+      }
+      be.Close();
+    }
+  }
+}
